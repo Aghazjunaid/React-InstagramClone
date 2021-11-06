c34823c508db47ba90b73d1a0ff2fc131e10944b
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-11-06 21:28:04 +0530

    profile api

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Header from '../../helpers/Header/Header'
 import {Container, Row, Col, Image} from 'react-bootstrap';
 import './profile.css';
 import { NavLink, Link } from "react-router-dom";
 
 function Profile() {
+    const [data,setData] = useState({})
+    const [post,postData] = useState([])
+
+    useEffect(()=>{
+        getProfile()
+        getAllPosts()
+    },[])
+
+    async function getProfile(){
+        let userData = localStorage.getItem('userData');
+        let userdataNew = JSON.parse(userData)
+        debugger
+        let result = await fetch("http://localhost:5000/ownProfile",{
+            method: 'GET',
+            headers: {
+                "Content-Type":"application/json",
+                "Accept": "application/json",
+                "authorization": userdataNew.data.token
+            },
+        })
+        debugger
+        result = await result.json();
+        setData(result.data)
+        console.log("result",result)
+    }
+
+    async function getAllPosts(){
+        let userData = localStorage.getItem('userData');
+        let userdataNew = JSON.parse(userData)
+        debugger
+        let result = await fetch("http://localhost:5000/getAllUsersPosts",{
+            method: 'GET',
+            headers: {
+                "Content-Type":"application/json",
+                "Accept": "application/json",
+                "authorization": userdataNew.data.token
+            },
+        })
+        debugger
+        result = await result.json();
+        postData(result.data)
+        console.log("postresult",result)
+    }
+
+
+
     return (
         <>
             <Header/>
@@ -12,43 +58,35 @@ function Profile() {
                 <div className="profileBody">
                     <div className="profileTop">
                         <div>
-                            <Image className="profilePic img-fluid" src={`${process.env.PUBLIC_URL}/assets/dpimage.jpg`} roundedCircle />
+                            <Image className="profilePic img-fluid" src={data.profileImage} roundedCircle />
                         </div>
                         <div>
                             <Row className="profileEdit">
-                                <Col md={6}><div className="profileUsername">faisal_78625</div></Col>
+                                <Col md={6}><div className="profileUsername">{data.username}</div></Col>
                                 <Col md={6}><button className="editbutn">Edit Profile</button></Col>
                             </Row>
                             <div className="followers">
                                 <div>4 posts</div>
-                                <div>11 followers</div>    
-                                <div>23 following</div>        
+                                <div>{data.followers ? data.followers.length : 0} followers</div>    
+                                <div>{data.following ? data.following.length : 0} following</div>        
                             </div>
-                            <div className="ProfileName">Faisal Junaid</div>
+                            <div className="ProfileName">{data.fullname}</div>
                             <div className="bio">
-                                Official I'd
-                                Welcome to my world 🌍
-                                First crush my mom and dad
-                                Music addicted 🎧
-                                My life my choice ☺️
-                                Wish me on 31 July 🎂
-                                #HMS
+                                {data.bio}
                             </div>   
                         </div>
                     </div>
                     <hr/>
                     <Row style={{marginTop:"20px"}}>
-                        <Col><Image className="postPic img-fluid" src={`${process.env.PUBLIC_URL}/assets/dpimage.jpg`} rounded /></Col>
-                        <Col><Image className="postPic img-fluid" src={`${process.env.PUBLIC_URL}/assets/dpimage.jpg`} rounded /></Col>
-                        <Col><Image className="postPic img-fluid" src={`${process.env.PUBLIC_URL}/assets/dpimage.jpg`} rounded /></Col>
-                    </Row>
-                    <Row style={{marginTop:"20px"}}>
-                        <Col><Image className="postPic img-fluid" src={`${process.env.PUBLIC_URL}/assets/dpimage.jpg`} rounded /></Col>
-                        <Col><Image className="postPic img-fluid" src={`${process.env.PUBLIC_URL}/assets/dpimage.jpg`} rounded /></Col>
-                        <Col><Image className="postPic img-fluid" src={`${process.env.PUBLIC_URL}/assets/dpimage.jpg`} rounded /></Col>
+                        {
+                            post.map(ele => 
+                                <Link style={{ textDecoration: 'none' }} className="col-xs-12 m-2">
+                                    <Image className="postPic img-fluid" src={ele.image} rounded />
+                                </Link>
+                            )
+                        }
                     </Row>
                 </div>
-                
            </Container>
         </>
     )
