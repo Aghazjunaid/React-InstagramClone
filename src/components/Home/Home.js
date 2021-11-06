648b94c62bf0d5db91035cfe4207faea7bbfe87b
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-11-06 16:04:12 +0530

    misc upd

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../helpers/Header/Header'
 
 function Home() {
     return (
         <div>
+            <Header/>
             <h1>Home</h1>
         </div>
     )
